6f8827e9c414dafbb7a1e3d84a5da390f5f05b11
Surriya Gokul <23110324@iitgn.ac.in>
2025-07-08 15:11:33 +0530

    minor changes

--- Siamese-Graphormer/network/siamese.py
+++ Siamese-Graphormer/network/siamese.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from encoder import GraphTransformerEncoder
+from .encoder import GraphTransformerEncoder
 
 
 class SiameseGraphNetwork(nn.Module):
